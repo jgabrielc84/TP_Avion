c9a6e9dc57af9cdcbe04d28152c56eeb588a471f
Oriana <orianaph@gmail.com>
2019-05-14 15:41:53 -0300

    Testeando cosas

--- avion/src/avion.c
+++ avion/src/avion.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <string.h>
 
 #define LONG_IDENTIFICADOR 8
 #define LONG_MODELO 20
@@ -23,40 +24,76 @@ typedef struct{
 	char estado;
 }ST_AVION;
 
+
+
 int main(void) {
-	//Se crea un servidor con los siguientes datos:
-	struct sockaddr_in direccionServidor;
-	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = INADDR_ANY;
-	direccionServidor.sin_port = htons(8080);
 
-	int servidor = socket(AF_INET, SOCK_STREAM, 0); //se pide un socket que devuelve un valor
+	    struct sockaddr_in direccionServidor;
+	    direccionServidor.sin_family = AF_INET;
+	    direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	    direccionServidor.sin_port = htons(8080);
 
-	//Linea para poder probar el socket sin que el sistema te deje en espera por 2 min
-	int activado = 1;
-	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+	    int cliente = socket(PF_INET, SOCK_STREAM, 0);
 
-	//Bind: asocia el socket creado con la direccion del servidor creado al principio
-	if(bind(servidor, (void *) &direccionServidor, sizeof(direccionServidor)) != 0){ //Valida el bind para saber si el sistema le da el puerto seleccionado o no
-		perror("Fallo el Bind");
-		return 1;
+
+	    //Linea para poder probar el socket sin que el sistema te deje en espera por 2 min
+	    int activado = 1;
+	    setsockopt(cliente, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+
+	    int choice = 0;
+
+
+
+
+
+	     do{
+		     printf("Menu\n\n");
+		     printf("1. Conectar a aeropuerto\n");
+		     printf("2. Pedir pista\n");
+		     printf("3. Exit\n");
+		     printf("4. ENviar mensaje\n");
+		     scanf("%d",&choice);
+
+	    	 switch (choice) {
+				 case 1:
+					 if(connect(cliente, (void  *) &direccionServidor, sizeof(direccionServidor)) != 0){
+							 perror("No se pudo conectar");
+							 return 1;
+					 }
+					 printf("Conectado");
+
+					  break;
+				 case 2:
+					 printf("TOdavia no\n");
+					  break;
+				 case 3:
+					 printf("Goodbye\n");
+					  break;
+				 case 4:
+					    while (1) {
+					        char mensaje[1000];
+					        scanf("%s", mensaje);
+
+					        send(cliente, mensaje, strlen(mensaje), 0);
+
+					    }
+					 break;
+				 default: printf("Wrong Choice. Enter again\n");
+					  break;
+			 }
+
+
+	     } while (choice != 3);
+
+
+
+
+
+
+
+
+	    return 0;
 	}
 
-	printf("Estoy escuchando!\n");
-	listen(servidor, 100); //Se queda escuchando por si se conectan al servidor
 
-	//Se crea un cliente para poder identificar quien se conecta
-	struct sockaddr_in direccionCliente;
-	unsigned int tamanioDireccion;
-
-	int cliente = accept(servidor, (void *) &direccionCliente, &tamanioDireccion);
-
-	printf("Recibi una conexion en %d!!\n", cliente);
-
-	//Envia contestacion al cliente
-	send(cliente, "Mensaje Recibido", 17, 0);
-
-	for(;;);
-
-	return EXIT_SUCCESS;
-}
